8ba99720f72581cbfebe9f0b8d095c4cd101c8a2
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-02 00:12:49 +0530

    Geometry Commit.

--- src/com/java/Fangeouc1/GeometryUC1.java
+++ src/com/java/Fangeouc1/GeometryUC1.java
@@ -2,7 +2,7 @@ package com.java.Fangeouc1;
 
 import java.util.Scanner;
 
-public class Main {
+public class GeometryUC1 {
 
     public static void main(String[] args) {
 	// write your code here
---
+++ src/com/java/Fangeouc1/GeometryUC2.java
@@ -0,0 +1,8 @@
+package com.java.Fangeouc1;
+
+import java.awt.geom.Line2D;
+
+public class GeometryUC2 {
+
+
+}
